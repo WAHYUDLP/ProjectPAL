d65538519cfad6eac40a5076bba8056d5f6f99cc
WAHYUDLP <149256633+WAHYUDLP@users.noreply.github.com>
2025-05-30 17:13:52 +0700

    terbaru

--- db.php
+++ db.php
@@ -6,8 +6,8 @@
 
 $host = 'db'; // Ganti dari 127.0.0.1 ke nama service di docker-compose
 $db = 'bloodwellnessaccount';
-$user = 'root';
-$pass = '';
+$user = 'blooduser';
+$pass = 'bloodpass123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
